ad7234cdfd561c5010787c6927b8a2d2592d276b
Sanjiban Dey <sanjibandey2000@gmail.com>
2025-10-14 09:03:52 +0530

    React router dom setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 const App = () => {
+
+  const router=createBrowserRouter([
+    {
+      path:"/",
+      children:[
+
+      ]
+    }
+  ]);
+
   return (
-    <div>App</div>
+    <RouterProvider router={router}/>
   )
 }
 
